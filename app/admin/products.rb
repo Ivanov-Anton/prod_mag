87fdd00ebf2002de305f1904076c96b5c893eae0
Anton-Ivanov <anton.i@hey.com>
2024-03-13 21:10:37 +0200

    refactor Product resource

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -7,26 +7,45 @@ ActiveAdmin.register Prod::Product, as: 'Product' do
     [:level_of_quality, :name, :orders_count, :price, :product_category_id, :quantity_in_stock, :quantity_sold, :department_id, :type_of_measure]
   end
 
-  index title: 'Товары' do
-    actions
+  index do
     selectable_column
     id_column
-    column 'Имя', &:name
-    column 'Кол-во проданых товаров', &:orders_count
-    column 'Цена', &:price
-    column 'Кол-во доступных продуктов', &:quantity_in_stock
-    column 'Кол-во проданых товаров', &:quantity_sold
-    column 'Отдел', &:department_ids
-    column 'Уровень качества', &:level_of_quality
-    column 'Мера', &:type_of_measure
+    column :name
+    column :price
+    column :quantity_in_stock
+    column :quantity_sold
+    column :department
+    column :level_of_quality do |product|
+      Prod::Product::CONST::LEVEL_OF_QUALITIES.invert.fetch(product.level_of_quality)
+    end
+    column :type_of_measure do |product|
+      Prod::Product::CONST::TYPES_OF_MEASURE.invert.fetch(product.type_of_measure)
+    end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+    end
   end
 
   form do |f|
     f.inputs do
-      f.input :name
-      f.input :department_ids, as: :select, collection: Prod::Department.all, label: 'Department', input_html: { multiple: true }
+      f.input :name, input_html: { autofocus: :autofocus }
+      f.input :department, as: :searchable_select, collection: Prod::Department.all
+      f.input :level_of_quality, as: :searchable_select, collection: Prod::Product::CONST::LEVEL_OF_QUALITIES
+      f.input :type_of_measure, as: :searchable_select, collection: Prod::Product::CONST::TYPES_OF_MEASURE
+      f.input :price, as: :number, placeholder: 'Цена в грн', hint: 'Цена за один товар'
+      f.input :quantity_in_stock, as: :number, label: 'Кол-во'
+      f.input :product_category, as: :searchable_select, collection: Prod::ProductCategory.all
     end
 
-    f.actions
+    f.actions do
+      verb = f.object.persisted? ? 'Обновить' : 'Создать'
+      f.action :submit, label: "#{verb}"
+      f.cancel_link
+    end
   end
 end
