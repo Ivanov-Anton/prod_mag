b83e4d6854ced288d25ea134e5a4c988c11b05cc
Anton-Ivanov <anton.i@hey.com>
2024-03-13 21:45:33 +0200

    add form config for department resource

--- app/admin/departments.rb
+++ app/admin/departments.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Prod::Department, as: 'department' do
+  config.create_another = true
   permit_params do
     ["name"]
+  end
+
+  index do
+    selectable_column
+    column :name
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name, input_html: { autofocus: :autofocus }
+    end
+    f.actions do
+      verb = f.object.persisted? ? 'Редактировать' : 'Создать'
+      f.action :submit, label: verb
+      f.cancel_link
+    end
   end
 end
