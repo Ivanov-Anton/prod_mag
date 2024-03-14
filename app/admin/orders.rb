2f4591c2b6f1c4464677e4cecde485d40f0660e1
Anton-Ivanov <anton.i@hey.com>
2024-03-14 09:03:30 +0200

    add order page

---
+++ app/admin/orders.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Prod::Order, as: 'order' do
+  actions :index, :show, :create, :new
+  permit_params do
+    %w[quantity product_id]
+  end
+
+  index do
+    selectable_column
+    column :price
+    column :product
+    column :quantity
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :price
+      row :quantity
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :quantity, as: :number, input_html: { autofocus: :autofocus }
+      f.input :product, as: :searchable_select
+    end
+    f.actions do
+      verb = f.object.persisted? ? 'Редактировать' : 'Создать'
+      f.action :submit, label: verb
+      f.cancel_link
+    end
+  end
+end
--- app/models/prod/order.rb
+++ app/models/prod/order.rb
@@ -16,8 +16,16 @@ module Prod
   class Order < ApplicationRecord
     self.table_name = 'orders'
 
+    belongs_to :department, class_name: 'Prod::Department', optional: true
+    belongs_to :product_category, class_name: 'Prod::ProductCategory', optional: true
+    belongs_to :product, class_name: 'Prod::Product', optional: true
+
     def self.ransackable_attributes(auth_object = nil)
       %w[created_at updated_at id id_value level_of_quality price quantity department_id product_category_id product_id]
+    end
+
+    def self.ransackable_associations(auth_object = nil)
+      %w[department product product_category]
     end
   end
 end
