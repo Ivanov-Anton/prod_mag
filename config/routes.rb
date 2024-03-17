3cf64c81e0175d1bd86d98d005657d83da136e4a
Anton-Ivanov <anton.i@hey.com>
2024-03-17 12:03:50 +0200

    change default route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root to: "admin/dashboard#index"
+  root to: 'admin/products#index'
+
   ActiveAdmin.routes(self)
 end
