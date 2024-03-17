ca7ea2a1e4d98a4082d755e129b88cb483da9f0e
Anton-Ivanov <anton.i@hey.com>
2024-03-17 12:50:11 +0200

    disable filters for department page

--- app/admin/departments.rb
+++ app/admin/departments.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Prod::Department, as: 'department' do
+  actions :all
+  config.filters = false
   config.create_another = true
   permit_params do
     ["name"]
@@ -13,7 +15,10 @@ ActiveAdmin.register Prod::Department, as: 'department' do
       link_to "Кол-во товаров (#{department.products_count})", admin_products_path(q: { department_id_eq: department.id }), class: 'button'
     end
     column do |department|
-      link_to "Кол-во заказов (#{department.orders_count})", admin_orders_path(q: { department_id_eq: department.id }), class: 'button'
+      link_to "Кол-во продаж (#{department.orders_count})", admin_orders_path(q: { department_id_eq: department.id }), class: 'button'
+    end
+    column('Всего прибыли', sortable: 'total') do |department|
+      department.orders.sum('price * quantity')
     end
     actions
   end
@@ -30,7 +35,9 @@ ActiveAdmin.register Prod::Department, as: 'department' do
     f.inputs do
       f.input :name, input_html: { autofocus: :autofocus }
     end
+
     f.actions do
+      f.add_create_another_checkbox
       verb = f.object.persisted? ? 'Редактировать' : 'Создать'
       f.action :submit, label: verb
       f.cancel_link
