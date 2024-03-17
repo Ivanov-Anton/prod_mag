41f9eafc388f15128fbf316314bdb85eeeb83428
Anton-Ivanov <anton.i@hey.com>
2024-03-17 12:47:15 +0200

    delete patch in favour active admin config (footer config)

--- config/initializers/active_admin_footer_patch.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ActiveAdmin::Views::Footer
-  def footer_text
-    'Програмное обеспечение для Директора продовольственного магазина, версия 1.0.0'
-  end
-end
