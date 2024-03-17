e1d3b6d54ef02a3e5c0a1ab54b3b167fbb998afd
Anton-Ivanov <anton.i@hey.com>
2024-03-17 12:09:03 +0200

    add cost_of_goods_for_replenishment page

---
+++ app/admin/cost_of_goods_for_replenishment.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Prod::Product, as: 'cost_of_goods_for_replenishment' do
+  actions :index
+  menu false
+  config.filters = false
+
+  controller do
+    def scoped_collection
+      super.where('quantity_in_stock < 10')
+    end
+  end
+
+  index download_links: false, title: 'Стоимость товаров для пополнения' do
+    actions { |product| link_to 'Добавить товар', edit_admin_product_path(product) }
+    column :name
+    column :quantity_in_stock
+    column 'Для того что закупить товар необходимо' do |product|
+      t_of_measure = Prod::Product::CONST::TYPES_OF_MEASURE.invert.fetch(product.type_of_measure)
+      "#{product.price * product.size_of_batch} грн. (для закупки #{product.size_of_batch} #{t_of_measure.downcase} товара)"
+    end
+  end
+end
