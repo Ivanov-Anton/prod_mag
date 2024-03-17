7aefc3e3b2b3dec736180c43f19571dfe30b0eed
Anton-Ivanov <anton.i@hey.com>
2024-03-17 12:05:30 +0200

    add patch to active admin gem to introduce thr new footer

---
+++ config/initializers/active_admin_footer_patch.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class ActiveAdmin::Views::Footer
+  def footer_text
+    'Програмное обеспечение для Директора продовольственного магазина, версия 1.0.0'
+  end
+end
